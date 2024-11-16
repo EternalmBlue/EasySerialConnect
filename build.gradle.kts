8e040628f0e09d2dd03541994459309f89ed1eaa
EternalBlue <2875002651@qq.com>
2024-11-17 02:26:16 +0800

    完成

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
 }
 
 group = "fun.eternalblue"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -13,7 +12,6 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation("com.fazecast:jSerialComm:2.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
 
 tasks.test {
